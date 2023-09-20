13330169a4381ed31d76c2ec5426525d9e94f0ab
Joules404 <joules.8122@gmail.com>
2023-09-19 21:11:44 -0400

    applying binary search multiple times

---
+++ leetcode/firstlastsa.java
@@ -0,0 +1,34 @@
+package leetcode;
+public class firstlastsa{
+    public static void main(String[] args) {
+        //System.out.println("hello world does this work");
+        int[] nums = {5,7,7,8,8,8,10};
+        // System.out.println(binarysearch(nums, 8));
+        int[] vals = searchRange(nums, 7);
+        System.out.println(vals[0] + " " + vals[1]);
+    }
+
+    static int[] searchRange(int[] nums, int target){
+        int ret[] = {-1,-1};
+        int start = inner(nums,target,true);
+        int end = inner(nums,target,false);
+        ret[0] = start;
+        ret[1] = end;
+        return ret;
+    }
+    static int inner(int[] nums, int target, boolean startIndex){
+        int ret = 0;;
+        int left = 0; int right = nums.length-1;
+        while(left <= right){
+            int middle = left + (right-left)/2;
+            if(target > nums[middle]) left = middle + 1;
+            else if(target < nums[middle]) right = middle - 1;
+            else{
+                ret = middle;
+                if(startIndex)right = middle - 1;
+                else left = middle + 1;
+            }
+        }
+        return ret;
+    }
+}
